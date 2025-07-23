e88e7f784af9f09b1777a76841c8d9552adc3ad6
Kleiton Albuquerque <kleiton2102@gmail.com>
2025-07-22 22:17:49 -0300

    Fazendo a Lista de Tarefas funcionar com conceitos do React

--- components/Task.jsx
+++ components/Task.jsx
@@ -1,12 +1,19 @@
 import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { colors } from "../constants/colors";
 
-export default function Task({ text }) {
+export default function Task({ text, initialCompleted }) {
+  const [completed, setCompleted] = useState(initialCompleted);
+
   return (
     <View style={style.rowContainer}>
-      <Pressable>
-        <Ionicons name="checkmark-circle" size={32} color={colors.primary} />
+      <Pressable onPress={() => setCompleted(!completed)}>
+        <Ionicons
+          name="checkmark-circle"
+          size={32}
+          color={completed ? colors.primary : "gray"}
+        />
       </Pressable>
       <Text>{text}</Text>
     </View>
